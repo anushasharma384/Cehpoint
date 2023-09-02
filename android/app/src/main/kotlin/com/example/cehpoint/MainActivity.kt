569323ea2411985d9f9826f9d27420812a359a92
anushasharma384 <sharmaanusha384@gmail.com>
2023-09-02 22:21:49 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/cehpoint/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cehpoint
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Screens/start.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StartPage extends StatefulWidget {
+  const StartPage({super.key, required String title});
+
+  @override
+  State<StartPage> createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
